28b8432d6334e1065a64f6b8ea755419d146ed17
새이름 <@LAPTOP-HL295IKH>
2022-11-08 11:03:58 +0900

    work

--- TeamE/src/Hi.java
+++ TeamE/src/Hi.java
@@ -4,6 +4,7 @@ public class Hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi 아임 무건!!!!!!");
+		System.out.println("hi 아임 민혁!!!!!!");
 	}
 
 }
