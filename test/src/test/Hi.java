5e2bb191d49a47930289848a8ab0253f709c2298
새이름 <@LAPTOP-HL295IKH>
2022-11-08 10:37:18 +0900

    sin

--- test/src/test/Hi.java
+++ test/src/test/Hi.java
@@ -6,6 +6,7 @@ public class Hi {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요! 송무건입니다");
 		System.out.println("안녕하세요! 최민혁입니다");
+		System.out.println("안녕하세요! 신유식입니다");
 	}
 
 }
