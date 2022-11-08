503045c3738efd293ab2e7501c92663577c8d550
새이름 <@LAPTOP-HL295IKH>
2022-11-08 10:33:35 +0900

    hi

--- test/src/test/Hi.java
+++ test/src/test/Hi.java
@@ -5,6 +5,7 @@ public class Hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요! 송무건입니다");
+		System.out.println("안녕하세요! 최민혁입니다");
 	}
 
 }
