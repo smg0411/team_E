14e257566ce1f6db0855984ad35ae8d2dae26011
sys73 <sys73@DESKTOP-07PQ55V>
2022-11-10 20:33:27 +0900

    변경

--- TeamE/src/Hi.java
+++ TeamE/src/Hi.java
@@ -7,5 +7,6 @@ public class Hi {
 		System.out.println("hi 아임 민혁!!!!!!");
 		System.out.println("hi 아임 민우!!!!!!");
 		System.out.println("hi 아임 유식!!!!!!");
+		System.out.println("프로그래밍 기초 프로젝트");
 	}
 }
