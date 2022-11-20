d6a55a3e3b52fa318f1a58ce80097bf4f7bea53f
새이름 <@LAPTOP-HL295IKH>
2022-11-20 17:48:44 +0900

    PACK MAN 이미지 커밋

--- PAC_MAN/src/PAC_MAN/MainFrame.java
+++ PAC_MAN/src/PAC_MAN/MainFrame.java
@@ -1,6 +1,7 @@
 package PAC_MAN;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
@@ -9,21 +10,22 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame implements ComponentListener {
-
+	private Screen screen;
 	
 	public MainFrame() {
-		setSize(1000, 700);
-		setTitle("PAC MAN!");
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setVisible(true);
-	}
-	
-	
-	
+		this.screen = new Screen();
 
-	
+		add(screen);
+		
+		setSize(1000, 700);                       // 화면 크기 설정
+		setTitle("PACK MAN!");                     // 텍스트 타이틀 
+		setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);                      //창의 크기를 변경하지 못하게 
+		setLocationRelativeTo(null);              //창이 가운데 나오게
+		setVisible(true); 						// 화면 보이게 설정
+		
+	}
 	
 	@Override
 	public void componentResized(ComponentEvent e) {
--- PAC_MAN/src/PAC_MAN/Screen.java
+++ PAC_MAN/src/PAC_MAN/Screen.java
@@ -1,11 +1,34 @@
 package PAC_MAN;
 
 import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.ImageIcon;
+
 public class Screen extends Canvas implements KeyListener{
 
+	private Image PMImg;
+
+	public Screen() {
+		ImageIcon icon = new ImageIcon("src/resource/PM.jpg");
+		this.PMImg = icon.getImage();
+		
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(PMImg, 0, 0, this);
+	}
+	
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		paint(g);
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
