f428e83e7fde4bbc69c275ab9d3c53588f1bdd8c
smg0411 <cookies0808@naver.com>
2022-12-09 10:08:44 +0900

    Merge branch 'main' of https://github.com/smg0411/team_E.git into main

--- PAC_MAN/src/PAC_MAN/Screen.java
+++ PAC_MAN/src/PAC_MAN/Screen.java
@@ -13,12 +13,10 @@ public class Screen extends Canvas implements KeyListener {
 	private Graphics bufferGraphics;
 	private Image offScreen;
 	private Dimension dim;
-	private Image PMImg;
 	private int stage = 1;
 
 	public Screen() {
-		ImageIcon icon = new ImageIcon("resource/PM.jpg");
-		this.PMImg = icon.getImage();
+		
 	}
 
 	private void initBuffer() {
@@ -29,7 +27,7 @@ public class Screen extends Canvas implements KeyListener {
 
 	@Override
 	public void paint(Graphics g) {
-		g.drawImage(PMImg, 0, 0, this);
+		
 	}
 	
 	@Override
